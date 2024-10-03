108381434bdcf2828115ad9acd9dec84b8e3cfef
Ducu Stoica <118366030+thatduckoverthere@users.noreply.github.com>
2024-10-03 11:00:01 +0300

    Update gr_gen.py

--- gr_gen.py
+++ gr_gen.py
@@ -9,7 +9,7 @@ file_meta_data = file + "#" + hashlib.md5(open(file,'rb').read()).hexdigest()
 QR_collection.append(qrcode.make(file_meta_data, version=10, error_correction=qrcode.ERROR_CORRECT_H, border=4))
 
 for chunk in chuncks.Chuncker(filename=file):
-    QR_collection.append(qrcode.make(chunk, version=10, error_correction=qrcode.ERROR_CORRECT_H, border=4))
+    QR_collection.append(qrcode.make(chunk, version=10, error_correction=qrcode.ERROR_CORRECT_H, border=4, mode='binary'))
     
 for index in range(len(QR_collection)):
     img=QR_collection[index]
